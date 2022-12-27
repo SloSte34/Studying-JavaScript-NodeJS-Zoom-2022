220f7e8b7eec216092f51ddd54c056b3882908d3
minsu <cocacolazero024@gmail.com>
2022-12-27 19:25:39 +0900

    Frontend Setup - static

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
-//pug settings
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 //pug settings
+
+app.use('/public', express.static(__dirname + '/public'));
+//user가 /public으로 가게 되면 __dirname + /'public' 폴더를 보여주게 한다.
 
 app.get('/', (req, res) => res.render('home'));
 //Routing Setting
