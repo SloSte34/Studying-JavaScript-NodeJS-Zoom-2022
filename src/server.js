ebf928ac78bdfa19cb4dfaf0af066e4e906bbd35
minsu <cocacolazero024@gmail.com>
2022-12-27 19:19:02 +0900

    Front Setup - Routing

--- src/server.js
+++ src/server.js
@@ -4,8 +4,11 @@ const app = express();
 
 //pug settings
 app.set('view engine', 'pug');
-app.set('views', __dirname + '/src/views');
+app.set('views', __dirname + '/views');
 //pug settings
+
+app.get('/', (req, res) => res.render('home'));
+//Routing Setting
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 app.listen(3000, handleListen);
